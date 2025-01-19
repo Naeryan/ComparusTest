4eec2f5f0af817dbd10adefd33ac78c8533d7f59
IgorKornienko <i.k.skrillax@gmail.com>
2025-01-19 12:55:49 +0200

    db init

---
+++ db-init/data-base-2.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE "data-base-2";
+\connect "data-base-2"
+
+CREATE TABLE user_table (
+    ldap_login  VARCHAR PRIMARY KEY,
+    name        VARCHAR,
+    surname     VARCHAR
+);
+
+INSERT INTO user_table VALUES
+    ('user-ldap-1', 'Eren', 'Jeger'),
+    ('user-ldap-2', 'Mikasa', 'Ackerman');
